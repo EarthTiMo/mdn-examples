1ce640ce3aa516c75b4bfaf5049ee54023014dfb
Roy_T <tiandejian@126.com>
2018-11-12 07:30:20 +0800

    under construction

--- javascript/apis/drawing-graphics/tetris-game/main.js
+++ javascript/apis/drawing-graphics/tetris-game/main.js
@@ -51,15 +51,16 @@ let width, height;   // 窗口的尺寸
 let rem;             // 相对单位，等于单个方块的宽度
 let layout;          // detail | regular | slim
 let score = 0, lines = 0, level = 0;
-let blockCount = [0, 0, 0, 0, 0, 0, 0, 0];
-let gamePanel = [];
-let curCol = 3, curRow = 0;
+let count = [0, 0, 0, 0, 0, 0, 0, 0];
+let panel = [];
+let currentCol = 3, currentRow = 0;
 let current, next = newBlock();
 
-initGamePanel();
+initPanel();
 pushBlock();
 setLayout();
 window.onresize = setLayout;
+document.onkeydown = moveBlock;
 
 function setSizeEnv() {
     width = canvas.width = window.innerWidth;
@@ -98,73 +99,69 @@ function setGameBox() {
 
     ctx.save();
     ctx.fillStyle = COLOR[level].FADE;
-    ctx.fillRect(X - 1, Y - 1, rem*10 + 1, rem*20 + 1);
+    ctx.fillRect(X - 2, Y - 2, rem*10 + 2, rem*20 + 2);
 
-    for (let i = 0; i < 20; i++) {
-        for (let j = 0; j < 10; j++) {
-            if (gamePanel[i][j] === 2) {
+    for (let i = 0; i < 20; i++)
+        for (let j = 0; j < 10; j++)
+            if (panel[i][j] === 1) {
                 ctx.fillStyle = COLOR[level].MEDIUM;
-                ctx.fillRect(X + rem*j,  Y + rem*i, rem, rem);
+                ctx.fillRect(X + rem*j - 1,  Y + rem*i - 1, rem + 1, rem + 1);
             }
-        }
-    }
 
     drawBlock(current.number, current.sequence, 1,
-              X + rem*curCol + 1, Y + rem*curRow + 1);
+              X + rem*currentCol, Y + rem*currentRow);
 
     ctx.restore();
 }
 
 
 function setInfoBox() {
-    let X = width/2 + rem*3.5,
-        Y = rem*4;
-    if (layout === 'detail')
-        X = width/2 - rem*2;
 
     ctx.save();
-    ctx.fillStyle = '#000';
-    ctx.fillRect(X, Y, rem*4, rem*15);
-    ctx.fillStyle = COLOR[level].FADE;
-    ctx.fillRect(X, Y + rem*6.5, rem*4, rem);
-    ctx.fillRect(X, Y + rem*9.5, rem*4, rem);
-    ctx.fillRect(X, Y + rem*12.5, rem*4, rem);
+    if (layout === 'detail' || layout === 'regular') {
+        let X = width / 2 + rem * 3.5,
+            Y = rem * 4;
+        if (layout === 'detail')
+            X = width / 2 - rem * 2;
 
-    ctx.fillStyle = COLOR[level].LIGHT;
-    ctx.font = rem + 'px 微软雅黑';
-    ctx.fillText('下一方块', X, Y);
-    ctx.fillText('当前关卡', X, Y + rem*6);
-    ctx.fillText('已消层数', X, Y + rem*9);
-    ctx.fillText('当前得分', X, Y + rem*12);
+        ctx.fillStyle = '#000';
+        ctx.fillRect(X, Y, rem * 4, rem * 15);
+        ctx.fillStyle = COLOR[level].FADE;
+        ctx.fillRect(X, Y + rem * 6.5, rem * 4, rem);
+        ctx.fillRect(X, Y + rem * 9.5, rem * 4, rem);
+        ctx.fillRect(X, Y + rem * 12.5, rem * 4, rem);
 
-    ctx.font = rem + 'px Consolas';
-    let numberX = (number) => X + rem * .08 + rem * .55 * (7 - String(number).length);
-    ctx.fillText(level, numberX(level), Y + rem * 7.3);
-    ctx.fillText(lines, numberX(lines), Y + rem * 10.3);
-    ctx.fillText(score, numberX(score), Y + rem * 13.3);
+        ctx.fillStyle = COLOR[level].LIGHT;
+        ctx.font = rem + 'px 微软雅黑';
+        ctx.fillText('下一方块', X, Y);
+        ctx.fillText('当前关卡', X, Y + rem * 6);
+        ctx.fillText('已消层数', X, Y + rem * 9);
+        ctx.fillText('当前得分', X, Y + rem * 12);
 
-    drawBlock(next.number, next.sequence, 1, X, Y + rem*.5);
-    ctx.restore();
-}
+        ctx.font = rem + 'px Consolas';
+        let numberX = (number) => X + rem * .08 + rem * .55 * (7 - String(number).length);
+        ctx.fillText(level, numberX(level), Y + rem * 7.3);
+        ctx.fillText(lines, numberX(lines), Y + rem * 10.3);
+        ctx.fillText(score, numberX(score), Y + rem * 13.3);
 
-function setSlimInfo() {
-    let rightX = width/2 + rem*2.8,
-        Y = height/2 - rem*9.4;
+        drawBlock(next.number, next.sequence, 1, X, Y + rem * .5);
+    } else {
+        let rightX = width/2 + rem*2.8,
+            Y = height/2 - rem*9.4;
+        ctx.fillStyle = COLOR[level].LIGHT;
+        ctx.font = rem*.5 + 'px consolas';
+        ctx.globalAlpha = .5;
+        ctx.fillText(lines, width/2 - rem*4.8, Y);
 
-    ctx.save();
-    ctx.fillStyle = COLOR[level].LIGHT;
-    ctx.font = rem*.5 + 'px consolas';
-    ctx.fillText(lines, width/2 - rem*4.8, Y);
+        ctx.font = rem*.5 + 'px 微软雅黑';
+        ctx.fillText('下一方块', rightX, Y);
 
-    ctx.font = rem*.5 + 'px 微软雅黑';
-    ctx.fillText('下一方块', rightX, Y);
-
-    drawBlock(next.number, next.sequence, .5, rightX, Y + rem*.2);
+        drawBlock(next.number, next.sequence, .5, rightX, Y + rem*.2);
+    }
     ctx.restore();
 }
 
 function setStatBox() {
-    ctx.save();
     if (layout === 'detail') {
         let statX = width/2 + rem*3,
             blockY = height - rem*4;
@@ -184,6 +181,8 @@ function setStatBox() {
         drawBlock(6, 1, .5, statX + rem*7.05,  blockY);
         drawBlock(7, 1, .5, statX + rem*8.45,  blockY);
 
+        ctx.fillStyle = COLOR[1].DARK;
+
         ctx.restore();
     }
 }
@@ -192,13 +191,13 @@ function setStatBox() {
 function drawBlock(number, sequence, size, x, y) {
     ctx.save();
     ctx.fillStyle = COLOR[number].MEDIUM;
-    for (let i = 0; i < BLOCKS[number][sequence].length; i++) {
-        for (let j = 0; j < BLOCKS[number][sequence][0].length; j++) {
-            if (BLOCKS[number][sequence][i][j] === 1) {
-                ctx.fillRect(x + j*rem*size-1, y + i*rem*size-1, rem*size+1, rem*size+1);
+    for (let i = 0; i < blockHeight(number, sequence); i++)
+        for (let j = 0; j < blockWidth(number, sequence); j++) {
+            if (blockMatrix(number, sequence)[i][j] === 1) {
+                ctx.fillRect(x + j*rem*size-1, y + i*rem*size-1,
+                             rem*size+1, rem*size+1);
             }
         }
-    }
     ctx.restore();
 }
 
@@ -206,40 +205,74 @@ function game() {
 
 }
 
-function initGamePanel() {
+function initPanel() {
     for (let i = 0; i < 20; i++) {
-        let current = [];
+        let cur = [];
         for (let j = 0; j < 10; j++) {
-            current.push(0);
+            cur.push(0);
         }
-        gamePanel.push(current);
+        panel.push(cur);
     }
 }
 
 function pushBlock() {
+    currentRow = 0;
+    currentCol = 3;
     current = next;
-    next = newBlock();
-    for (let i = 0; i < BLOCKS[current.number][current.sequence].length; i++) {
-        for (let j = 0; j < BLOCKS[current.number][current.sequence][0].length; j++) {
-            if (BLOCKS[current.number][current.sequence][i][j] === 1) {
-                gamePanel[i][j+3] = 1;
+    if ( ! reachBottom() ) {
+
+        next = newBlock();
+
+        count[0]++;
+        count[current.number]++;
+        return true;
+    }
+    else return false;
+}
+
+function acceptBlock() {
+    for (let i = 0; i < blockHeight(); i++)
+        for (let j = 0; j < blockWidth(); j++)
+             if (panel[currentRow+i][currentCol+j] === 0)
+                 panel[currentRow+i][currentCol+j] = blockMatrix()[i][j];
+}
+
+function moveBlock(e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            if ( !reachLeftEdge() )
+                currentCol--;
+            break;
+        case 'ArrowRight':
+            if ( !reachRightEdge() )
+                currentCol++;
+            break;
+        case 'ArrowDown':
+            if ( !reachBottom() )
+                currentRow++;
+            break;
+        case 'ArrowUp':
+            let temp = current.sequence++;
+            current.sequence %= BLOCKS[current.number].length;
+          //  if ( reachBottom() )
+            //    currentRow -= blockHeight() - (20 - currentRow);
+
+            if ( reachRightEdge() ) {
+                let tempCol = currentCol;
+                while ( !reachLeftEdge() )
+                    currentCol--;
+                while ( !reachRightEdge() )
+                    currentCol++;
+               // if ( currentCol === tempCol )   // 当前横向空间不够方块进行转体
+                //    current.sequence = temp;    // 方块保持不变
             }
-        }
+            break;
+        case 'Enter':
+            acceptBlock();
+            pushBlock();
+            break;
     }
-    blockCount[0]++;
-    blockCount[current.number]++;
-}
-
-function moveBlockLeft() {
-    if (current.X > 0) {
-        current.X--;
-    }
-}
-
-function moveBlockRight() {
-}
-
-function downBlock() {
+    setLayout();
 }
 
 
@@ -252,16 +285,50 @@ function newBlock() {
     };
 }
 
-function fit(block, row, col) {
-    if ( row < 0 || row + BLOCKS[block.number][block.sequence].length > 20 ||
-         col < 0 || col + BLOCKS[block.number][block.sequence][0].length > 10 )
-        return false;
-    for (let i = row; i < row + BLOCKS[block.number][block.sequence].length; i++)
-        for (let j = col; j < col + BLOCKS[block.number][block.sequence][0].length; j++) {
-            if (BLOCKS[i][j] === 1 && gamePanel[i][j] === 2)
-                return false;
+function reachLeftEdge() {
+    if (currentCol <= 0)
+        return true;
+    for (let i = 0; i < blockHeight(); i++)
+        for (let j = 0; j < blockWidth(); j++) {
+            if ( blockMatrix()[i][j] +
+                 panel[currentRow+i][currentCol+j-1] === 2)
+                return true;
         }
-    return true;
+    return false;
+}
+
+function reachRightEdge() {
+    if (currentCol + blockWidth() >= 10)
+        return true;
+    for (let i = 0; i < blockHeight(); i++)
+        for (let j = blockWidth()-1; j >= 0; j--)
+            if ( blockMatrix()[i][j] +
+                 panel[currentRow+i][currentCol+j+1] === 2 )
+                return true;
+    return false;
+}
+
+function reachBottom() {
+    if (currentRow + blockHeight() >= 20)
+        return true;
+    for (let i = 0; i < blockWidth(); i++)
+        if ( blockMatrix()[blockHeight()-1][i] +
+             panel[currentRow+blockHeight()][currentCol+i] === 2 )
+            return true;
+    return false;
+
+}
+
+function blockWidth(number = current.number, sequence = current.sequence) {
+    return BLOCKS[number][sequence][0].length;
+}
+
+function blockHeight(number = current.number, sequence = current.sequence) {
+    return BLOCKS[number][sequence].length;
+}
+
+function blockMatrix(number = current.number, sequence = current.sequence) {
+    return BLOCKS[number][sequence];
 }
 
 function rand(from, to) {
@@ -272,10 +339,6 @@ function setLayout() {
     setSizeEnv();
     setBackground();
     setGameBox();
-    if (layout === 'detail' || layout === 'regular')
-        setInfoBox();
-    else
-        setSlimInfo();
+    setInfoBox();
     setStatBox();
 }
-
