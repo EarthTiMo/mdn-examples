cbb007571862c313569e499d008235700130bf4b
Roy-Tian <tiandejian@126.com>
2018-11-22 00:59:42 +0800

    update

--- introduction-to-js/script.js
+++ introduction-to-js/script.js
@@ -6,6 +6,6 @@ function createParagraph() {
 
 var buttons = document.querySelectorAll('button');
 
-for(var i = 0; i < buttons.length ; i++) {
+for(let i = 0; i < buttons.length ; i++) {
   buttons[i].addEventListener('click', createParagraph);
 }
