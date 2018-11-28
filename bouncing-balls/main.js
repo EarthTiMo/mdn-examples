357f799af25c34330c9731e04c6b415318f2555a
Roy-Tian <tiandejian@126.com>
2018-11-28 11:09:14 +0800

    update

--- bouncing-balls/main.js
+++ bouncing-balls/main.js
@@ -5,26 +5,25 @@ const BALL_SPEED_MAX = 7;
 
 // 设定画布
 
-let canvas = document.querySelector('canvas');
-let ctx = canvas.getContext('2d');
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
 
-let width = canvas.width = window.innerWidth;
-let height = canvas.height = window.innerHeight;
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
 
 // 生成随机数的函数
 
 function random(min,max) {
-  let num = Math.floor(Math.random()*(max-min)) + min;
-  return num;
+  return Math.floor(Math.random()*(max-min)) + min;
 }
 
 // 生成随机颜色的函数
 
 function randomColor() {
-    return '#' +
-           random(0, 255).toString(16) +
-           random(0, 255).toString(16) +
-           random(0, 255).toString(16);
+    return 'rgb(' +
+           random(0, 255) + ', ' +
+           random(0, 255) + ', ' +
+           random(0, 255) + ')';
 }
 
 // 定义 Ball 构造器
@@ -73,11 +72,11 @@ Ball.prototype.update = function() {
 // 定义碰撞检测函数
 
 Ball.prototype.collisionDetect = function() {
-  for(let j = 0; j < balls.length; j++) {
+  for(var j = 0; j < balls.length; j++) {
     if( ! (this === balls[j]) ) {
-      let dx = this.x - balls[j].x;
-      let dy = this.y - balls[j].y;
-      let distance = Math.sqrt(dx * dx + dy * dy);
+      var dx = this.x - balls[j].x;
+      var dy = this.y - balls[j].y;
+      var distance = Math.sqrt(dx * dx + dy * dy);
 
       if (distance < this.size + balls[j].size) {
         balls[j].color = this.color = randomColor();        
@@ -88,17 +87,17 @@ Ball.prototype.collisionDetect = function() {
 
 // 定义一个数组来保存所有的球
 
-let balls = [];
+var balls = [];
 
 // 定义一个循环来不停地播放
 
 function loop() {
-  ctx.fillStyle = '#00000040';
+  ctx.fillStyle = 'rgb(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
 
   while(balls.length < BALLS_COUNT) {
-    let size = random(BALL_SIZE_MIN, BALL_SIZE_MAX);
-    let ball = new Ball(
+    var size = random(BALL_SIZE_MIN, BALL_SIZE_MAX);
+    var ball = new Ball(
       // 为避免绘制错误，球至少离画布边缘球本身一倍宽度的距离
       random(0 + size, width - size),
       random(0 + size, height - size),
